6feee7abb8fc67f30505a6886209cf9584b9a523
master <balazsnoe@gmail.com>
2019-10-27 14:15:53 +0200

    Commit

--- app/src/test/java/com/example/blockchain/MainUnitTest.kt
+++ app/src/test/java/com/example/blockchain/MainUnitTest.kt
@@ -1,9 +1,5 @@
 package com.example.blockchain
 
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.blockchain.adapter.EntityAdapter
-import com.example.blockchain.data.CurrencyEntity
 import com.example.blockchain.ui.MainActivity
 import org.junit.Assert
 import org.junit.Before
@@ -29,5 +25,4 @@ class MainUnitTest {
     fun shouldNotBeNull(){
         Assert.assertNotNull(mainActivity)
     }
-
 }
